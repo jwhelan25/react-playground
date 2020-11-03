d267a2e6eb606d3823a3c6bc7840fbc22e616d30
Joshua Whelan <josh25whelan@gmail.com>
2020-11-03 16:05:39 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import Split from './composition/Split'
+import './App.css'
+import Tooltip from './composition/Tooltip'
+
+const firstTooltip = (
+  <Tooltip color='hotpink' message='tooltip message'>
+    ipsum
+  </Tooltip>
+)
+
+const secondTooltip = (
+  <Tooltip color='#126BCC' message='another tooltip message'>
+    oficiis
+  </Tooltip>
+)
+
 
 function App() {
   return (
     <main className='App'>
-      <Split />
+     <Split className='left' flexBasis={2}>
+        This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+        <br />
+        
+        <div>
+        {firstTooltip}
+        {secondTooltip}
+          </div>  
+      
+        <Tooltip message='one more tooltip message'>
+          Necessitatibus?
+        </Tooltip>
+      </Split>
+      <Split className='right'>
+        This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
+      </Split>
     </main>
   );
 }
